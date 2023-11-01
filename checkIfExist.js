3410c81fafbf0ecfbae85afa1aa2ecfdadd68b02
arromero4 <arromero0408@gmail.com>
2023-10-31 23:39:03 -0600

    1346. Check If N and Its Double Exist using JS

--- checkIfExist.js
+++ checkIfExist.js
@@ -29,6 +29,33 @@ Constraints:
 2 <= arr.length <= 500
 -103 <= arr[i] <= 103
 
+
+
+Example:
+arr = [10,2,5,3]
+i = 0 arr[i] = 10 arr[i]*2=20
+j = 0 arr[j] = 10 != 20
+j = 1 arr[j] = 2 != 20
+j = 2 arr[j] = 5 != 20
+j = 3 arr[j] = 3 != 20
+index = -1 continue
+
+i = 1 arr[i] = 2 arr[i]*2=4
+j = 0 arr[j] = 10 != 4
+j = 1 arr[j] = 2 != 4
+j = 2 arr[j] = 5 != 4
+j = 3 arr[j] = 3 != 4
+index = -1 continue
+
+i = 2 arr[i] = 5 arr[i]*2=10
+j = 0 arr[j] = 10 == 10
+index = 0
+
+index > -1 
+
+index = 0 != i = 2
+return true
+
 /**
  * @param {number[]} arr
  * @return {boolean}
