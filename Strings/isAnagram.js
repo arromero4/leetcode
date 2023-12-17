e6594bdba2cf169893706d10f5b1470ab63184da
arromero4 <arromero0408@gmail.com>
2023-12-17 00:52:47 -0600

    242. Valid Anagram using JavaScript

---
+++ Strings/isAnagram.js
@@ -0,0 +1,47 @@
+// 242. Valid Anagram
+// Solved
+// Easy
+// Topics
+// Companies
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+ 
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    // Verificar si la longitud de ambas palabras es la misma
+     if (s.length !== t.length) {
+         return false;
+     }
+ 
+     // Convertir las palabras a arreglos, ordenarlos y luego compararlos
+     const sortedS = s.split('').sort().join('');
+     const sortedT = t.split('').sort().join('');
+ 
+     // Verificar si las palabras ordenadas son iguales
+     return sortedS === sortedT;
+ };
