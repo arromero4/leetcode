0902d1102255290de2e935a9c63facf1c87b9d13
arromero4 <arromero0408@gmail.com>
2024-01-27 17:12:27 -0600

    28. Find the Index of the First using Dart

---
+++ Array_and_String/strStr.dart
@@ -0,0 +1,45 @@
+// 28. Find the Index of the First Occurrence in a String
+// Easy
+// Topics
+// Companies
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+// Example 1:
+
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+// Constraints:
+
+// 1 <= haystack.length, needle.length <= 104
+// haystack and needle consist of only lowercase English characters.
+
+void main() {
+  var haystack = "sadbutsad";
+  var needle = "sad";
+  print(strStr(haystack, needle));
+}
+
+int strStr(String haystack, String needle) {
+  if (needle == "") return 0;
+  for (int i = 0; i < (haystack.length + 1) - needle.length; i++) {
+    bool match = true;
+    for (int j = 0; j < needle.length; j++) {
+      if (haystack[i + j] != needle[j]) {
+        match = false;
+        break;
+      }
+    }
+    if (match) {
+      return i;
+    }
+  }
+  return -1;
+}
