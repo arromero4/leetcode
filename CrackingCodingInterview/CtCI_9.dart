f43d5d710dac87525ef042055413cf3c9e9f7105
arromero4 <arromero0408@gmail.com>
2024-02-15 00:46:14 -0600

    Cracking Coding Interview example using Dart

---
+++ CrackingCodingInterview/CtCI_9.dart
@@ -0,0 +1,15 @@
+void main() {
+  String str = "abcd";
+  permutation(str);
+}
+
+void permutation(String str, [String prefix = ""]) {
+  if (str.length == 0) {
+    print(prefix);
+  } else {
+    for (var i = 0; i < str.length; i++) {
+      String rem = str.substring(0, i) + str.substring(i + 1);
+      permutation(rem, prefix + str[i]);
+    }
+  }
+}
