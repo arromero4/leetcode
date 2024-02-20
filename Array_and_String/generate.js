2a7dc03d6465ecfc6890b5927b881ea797288a61
arromero4 <arromero0408@gmail.com>
2024-02-20 01:55:00 -0600

    118. Pascal's Triangle using JavaScript

--- Array_and_String/generate.js
+++ Array_and_String/generate.js
@@ -32,11 +32,15 @@ var generate = function(numRows) {
     for (let i = 1; i < numRows; i++) {
         let temp = [0].concat(res[res.length - 1], [0]);
         let row = []
-        
+
        for (let j = 0; j < temp.length - 1; j++) {
             row.push(temp[j] + temp[j + 1])
         }
+
         res.push(row);
     }
     return res;
 };
+
+let numRows = 5
+console.log(generate(numRows))
