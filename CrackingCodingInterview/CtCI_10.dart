0a2f2a29d7440000e0f90f3bc5489beef0f64e27
arromero4 <arromero0408@gmail.com>
2024-02-22 00:26:16 -0600

    Cracking Coding Interview example using Dart

---
+++ CrackingCodingInterview/CtCI_10.dart
@@ -0,0 +1,13 @@
+void main() {
+  int n = 5;
+  print(fib(n));
+}
+
+int fib(int n) {
+  if (n <= 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
