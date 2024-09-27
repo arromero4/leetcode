ff254fd5f82ba509ac3c51b97adf2b16676b5b4c
Andrés Reyes Romero <arromero0408@gmail.com>
2024-09-27 13:01:10 -0600

    771. Jewels and Stones using JavaScript

---
+++ Hash_Tables/numJewelsInStones.js
@@ -0,0 +1,51 @@
+// # 771. Jewels and Stones
+// # Solved
+// # Easy
+// # Topics
+// # Companies
+// # Hint
+// # You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+// # Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+// # Example 1:
+
+// # Input: jewels = "aA", stones = "aAAbbbb"
+// # Output: 3
+// # Example 2:
+
+// # Input: jewels = "z", stones = "ZZ"
+// # Output: 0
+ 
+
+// # Constraints:
+
+// # 1 <= jewels.length, stones.length <= 50
+// # jewels and stones consist of only English letters.
+// # All the characters of jewels are unique.
+
+// /**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let mapJewels =  new Map()
+    let mapStones =  new Map()
+    let count = 0    
+    for(let jewel of jewels){
+        mapJewels.set(jewel, (mapJewels.get(jewel) || 0) + 1)
+    }
+
+    for(let stone of stones){
+        mapStones.set(stone, (mapStones.get(stone) || 0) + 1)
+    }
+
+    for(let [key, value] of mapStones.entries()){
+        if(mapJewels.has(key)) count += value
+    }
+    return count
+
+};
